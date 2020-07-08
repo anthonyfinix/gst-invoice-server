78666919da417e2ef0dd74629d143857e9226b0a
Anthony Finix <anthonyfinix@gmail.com>
2020-07-08 12:09:25 +0530

    test build folder

---
+++ build/precache-manifest.9f676ff337c411fb87b9278a5d301074.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c6dd21245e5b021151b105ea442ca8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31a090d6d3612dcebb44",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0ed1736fa91be82e1c4",
+    "url": "/static/js/2.cdf4b4fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cdf4b4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a090d6d3612dcebb44",
+    "url": "/static/js/main.0bdd1d5d.chunk.js"
+  },
+  {
+    "revision": "9473ea17c386807a80e6",
+    "url": "/static/js/runtime-main.1262bfa0.js"
+  }
+]);
